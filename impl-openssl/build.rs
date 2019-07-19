c7ff8ca883e4532759bd5d0e63906970b93b8fa6
Kishorekumar Neelamegam <kishore@lendsmart.ai>
2019-07-19 13:39:06 +0530

    Update build.rs

--- impl-openssl/build.rs
+++ impl-openssl/build.rs
@@ -1,19 +1,12 @@
 use std::env;
 
 fn main() {
-    // HAS   
     match env::var("DEP_OPENSSL_VERSION") {
         Ok(ref v) if v == "101" => {
         }
-        Ok(ref v) if v == "102" => {
+        Ok(ref v) if v >= "102" => {
             println!("cargo:rustc-cfg=has_alpn");
-        }
-        Ok(ref v) if v == "110" => {
-            println!("cargo:rustc-cfg=has_alpn");
-        }
-        Ok(ref v) if v == "111" => {
-            println!("cargo:rustc-cfg=has_alpn");
-        }
+        }        
         _ => panic!("tls-api-openssl: Unable to detect OpenSSL version"),
     }
 }
