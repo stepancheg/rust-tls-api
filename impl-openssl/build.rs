8b4221ffec217fbbebaa66dae2f0fb38be732204
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-19 06:34:01 +0000

    Support libressl

--- impl-openssl/build.rs
+++ impl-openssl/build.rs
@@ -1,11 +1,27 @@
 use std::env;
 
 fn main() {
-    match env::var("DEP_OPENSSL_VERSION").as_deref() {
-        Ok("101") => {}
-        Ok(v) if v >= "102" => {
-            println!("cargo:rustc-cfg=has_alpn");
+    // Keep in sync with
+    // https://github.com/sfackler/rust-openssl/blob/master/openssl-sys/build/main.rs
+
+    // `openssl` crate could export that instead of `openssl-sys`, we need
+    // https://github.com/rust-lang/cargo/issues/3544 for this.
+    match (
+        env::var("DEP_OPENSSL_VERSION").as_deref(),
+        env::var("DEP_OPENSSL_LIBRESSL_VERSION").as_deref(),
+    ) {
+        (_, Ok(v)) => {
+            assert_eq!(3, v.len());
+            if v >= "261" {
+                println!("cargo:rustc-cfg=has_alpn");
+            }
         }
-        _ => panic!("tls-api-openssl: Unable to detect OpenSSL version"),
+        (Ok(v), _) => {
+            assert_eq!(3, v.len());
+            if v >= "102" {
+                println!("cargo:rustc-cfg=has_alpn");
+            }
+        }
+        (Err(_), Err(_)) => panic!("tls-api-openssl: Unable to detect OpenSSL version"),
     }
 }
--- impl-openssl/tests/test.rs
+++ impl-openssl/tests/test.rs
@@ -7,7 +7,7 @@ fn test_google() {
 fn connect_bad_hostname() {
     tls_api_test::connect_bad_hostname::<tls_api_openssl::TlsConnector, _>(|err| {
         let debug = format!("{:?}", err);
-        assert!(debug.contains("certificate verify failed"), debug);
+        assert!(debug.contains("certificate verify failed"), "{}", debug);
     });
 }
 
