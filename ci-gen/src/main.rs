0ce83b314309ee3bab3ec97ccb311c272ecf99f6
Stepan Koltsov <stepan.koltsov@gmail.com>
2022-04-28 15:37:41 +0100

    Replace super-linter with mega-linter

--- ci-gen/src/main.rs
+++ ci-gen/src/main.rs
@@ -11,7 +11,7 @@ use gh_actions_gen::ghwf::Env;
 use gh_actions_gen::ghwf::Job;
 use gh_actions_gen::ghwf::Step;
 use gh_actions_gen::rustfmt::rustfmt_check_job;
-use gh_actions_gen::super_mega_linter::super_linter_job;
+use gh_actions_gen::super_mega_linter::mega_linter_job;
 
 fn crates_list() -> Vec<String> {
     assert!(Path::new("./ci-gen").exists());
@@ -122,7 +122,7 @@ fn jobs() -> Vec<Job> {
     r.push(cargo_doc_job());
 
     r.push(rustfmt_check_job());
-    r.push(super_linter_job());
+    r.push(mega_linter_job());
 
     r
 }
