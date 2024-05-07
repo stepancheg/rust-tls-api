1df0dbd64f5611e9fe9221698385bfc8ff2f4665
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-05-07 21:52:00 +0200

    Shutdown TLS streams
    When I tried updating to latest version of rustls, I've noticed the following test failure:
    
    ```
    rustls::Custom {
      kind: UnexpectedEof,
      error: "peer closed connection without sending TLS close_notify: https://docs.rs/rustls/latest/rustls/manual/_03_howto/index.html#unexpected-eof"
    }
    ```
    
    This is happening because in [rustls 0.21.2](https://github.com/rustls/rustls/releases/tag/v%2F0.21.2), they started differentiating between "unexpected and expected EOF in `Stream` and `StreamOwned`". This follows TLS standard, as the close_notify message must be sent before closing the connection.
    
    We weren't doing that I our test cases so they failed.
    
    But even when I added `AsyncWrite::poll_shutdown(socket).await` to the tests, they were still failing, since we weren't propagating this call to the underlying sync TLS streams, but just calling `.flush()`. The problem here was that there is no sync equivalent to `AsyncWrite::shutdown`.
    
    My solution was to add a new trait `WriteShutdown` that all sync TLS streams must implement. It turns out that OpenSSL, native TLS and rustls all have shutdown methods that they are expecting to be called.
    
    ---
    
    This is a breaking change, because there are streams that previous versions deemed correct, but newer versions would return an error on.
    
    This is fixes [vulnerability to truncation attacks](https://docs.rs/rustls/latest/rustls/manual/_03_howto/index.html#unexpected-eof).

--- impl-native-tls/src/stream.rs
+++ impl-native-tls/src/stream.rs
@@ -1,15 +1,18 @@
-use native_tls::TlsStream as native_tls_TlsStream;
 use std::fmt;
+use std::io;
+use std::io::Read;
+use std::io::Write;
 use std::marker::PhantomData;
 use tls_api::async_as_sync::AsyncIoAsSyncIo;
 use tls_api::async_as_sync::AsyncWrapperOps;
 use tls_api::async_as_sync::TlsStreamOverSyncIo;
+use tls_api::async_as_sync::WriteShutdown;
 use tls_api::spi_async_socket_impl_delegate;
 use tls_api::spi_tls_stream_over_sync_io_wrapper;
 use tls_api::AsyncSocket;
 use tls_api::ImplInfo;
 
-spi_tls_stream_over_sync_io_wrapper!(TlsStream, native_tls_TlsStream);
+spi_tls_stream_over_sync_io_wrapper!(TlsStream, NativeTlsStream);
 
 #[derive(Debug)]
 pub(crate) struct AsyncWrapperOpsImpl<S, A>(PhantomData<(S, A)>)
@@ -22,25 +25,79 @@ where
     S: fmt::Debug + Unpin + Send + 'static,
     A: AsyncSocket,
 {
-    type SyncWrapper = native_tls::TlsStream<AsyncIoAsSyncIo<A>>;
+    type SyncWrapper = NativeTlsStream<AsyncIoAsSyncIo<A>>;
 
     fn impl_info() -> ImplInfo {
         crate::info()
     }
 
     fn debug(w: &Self::SyncWrapper) -> &dyn fmt::Debug {
-        w
+        &w.0
     }
 
     fn get_mut(w: &mut Self::SyncWrapper) -> &mut AsyncIoAsSyncIo<A> {
-        w.get_mut()
+        w.0.get_mut()
     }
 
     fn get_ref(w: &Self::SyncWrapper) -> &AsyncIoAsSyncIo<A> {
-        w.get_ref()
+        w.0.get_ref()
     }
 
     fn get_alpn_protocol(w: &Self::SyncWrapper) -> anyhow::Result<Option<Vec<u8>>> {
-        w.negotiated_alpn().map_err(anyhow::Error::new)
+        w.0.negotiated_alpn().map_err(anyhow::Error::new)
+    }
+}
+
+pub(crate) struct NativeTlsStream<A: Read + Write>(pub(crate) native_tls::TlsStream<A>);
+
+impl<A: Read + Write> Write for NativeTlsStream<A> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.flush()
+    }
+
+    fn write_vectored(&mut self, bufs: &[io::IoSlice<'_>]) -> io::Result<usize> {
+        self.0.write_vectored(bufs)
+    }
+
+    fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
+        self.0.write_all(buf)
+    }
+
+    fn write_fmt(&mut self, fmt: fmt::Arguments<'_>) -> io::Result<()> {
+        self.0.write_fmt(fmt)
+    }
+}
+
+impl<A: Read + Write> WriteShutdown for NativeTlsStream<A> {
+    fn shutdown(&mut self) -> Result<(), io::Error> {
+        self.flush()?;
+        self.0.shutdown()?;
+        Ok(())
+    }
+}
+
+impl<A: Read + Write> Read for NativeTlsStream<A> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.read(buf)
+    }
+
+    fn read_vectored(&mut self, bufs: &mut [io::IoSliceMut<'_>]) -> io::Result<usize> {
+        self.0.read_vectored(bufs)
+    }
+
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        self.0.read_to_end(buf)
+    }
+
+    fn read_to_string(&mut self, buf: &mut String) -> io::Result<usize> {
+        self.0.read_to_string(buf)
+    }
+
+    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        self.0.read_exact(buf)
     }
 }
